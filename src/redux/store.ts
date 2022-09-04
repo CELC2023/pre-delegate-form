48693e38500f509d9be8223c89f798f19355fe62
Robert Brown <brown.rjb75@gmail.com>
2022-09-04 13:58:40 -0600

    feat: add delegate reducer to redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import {configureStore } from '@reduxjs/toolkit'
+import delegateReducer from './delegateReducer'
 import preDelegateReducer from './preDelegateReducer'
 
 export const store = configureStore({
     reducer: {
-        preDelegate: preDelegateReducer
+        preDelegate: preDelegateReducer,
+        delegate: delegateReducer,
     }
 })
