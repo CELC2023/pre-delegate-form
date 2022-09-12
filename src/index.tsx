2cbf72d322ded1b2bece53a2677ffcbdb002b588
Robert Brown <brown.rjb75@gmail.com>
2022-09-12 15:19:21 -0700

    fix: update mobile support

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './redux/store';
-import reportWebVitals from './reportWebVitals';
-import './i18n';
-import ReactGA from 'react-ga';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
+import "./i18n";
+import ReactGA from "react-ga";
 const TRACKING_ID = "G-XH89E1JJ75";
 ReactGA.initialize(TRACKING_ID);
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
 root.render(
   <React.StrictMode>
     <Provider store={store}>
