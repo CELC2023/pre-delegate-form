0db6ce909b80f5ed4b8e195f2e0c484f742bfaa5
Robert Brown <brown.rjb75@gmail.com>
2022-09-27 00:02:07 -0600

    feat: personal information form

--- src/redux/delegateReducer.ts
+++ src/redux/delegateReducer.ts
@@ -9,7 +9,7 @@ export interface delegateSliceInterface {
     lastName: string,
     preferredName: string,
     pronouns: string,
-    dateOfBirth: Date | null,
+    dateOfBirth: string,
     cfesOfficer: boolean,
     yearOfStudy: number,
     major: string,
@@ -49,7 +49,7 @@ const initialState: delegateSliceInterface = {
     lastName: '',
     preferredName: '',
     pronouns: '',
-    dateOfBirth: null,
+    dateOfBirth: '',
     cfesOfficer: false,
     yearOfStudy: 0,
     major: '',
@@ -92,13 +92,29 @@ export const delegateSlice = createSlice({
         setLanguagesPreference: (state, action) => {
             state.languages = action.payload.languages;
             state.frenchCaseCompetition = action.payload.frenchCaseCompetition;
+        },
+        setPersonalInformation: (state, action) => {
+            state.email = action.payload.email;
+            state.firstName = action.payload.firstName;
+            state.lastName = action.payload.lastName;
+            state.preferredName = action.payload.preferredName;
+            state.pronouns = action.payload.pronouns;
+            state.phone = action.payload.phone;
+            state.dateOfBirth = action.payload.dateOfBirth;
         }
     }
 })
 
-export const { setDietaryRestrictions, setLanguagesPreference } = delegateSlice.actions;
+export const { setDietaryRestrictions, setLanguagesPreference, setPersonalInformation } = delegateSlice.actions;
 
 // selectors
+export const selectEmail = (state: {delegate: {email: string}}) => state.delegate.email;
+export const selectFirstName = (state: {delegate: {firstName: string}}) => state.delegate.firstName;
+export const selectLastName = (state: {delegate: {lastName: string}}) => state.delegate.lastName;
+export const selectPreferredName = (state: {delegate: {preferredName: string}}) => state.delegate.preferredName;
+export const selectPronouns = (state: {delegate: {pronouns: string}}) => state.delegate.pronouns;
+export const selectPhone = (state: {delegate: {phone: string}}) => state.delegate.phone;
+export const selectDateOfBirth = (state: {delegate: {dateOfBirth: string}}) => state.delegate.dateOfBirth;
 export const selectAllergies = (state: {delegate: { allergies: string}}) => state.delegate.allergies;
 export const selectDietaryRestrictions = (state: {delegate: { dietaryRestrictions: Array<string>}}) => state.delegate.dietaryRestrictions;
 export const selectLanguagesPreference = (state: {delegate: { languages: Array<string>}}) => state.delegate.languages;
