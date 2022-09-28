a3a1b4efdf94fb2df2cb1b4f5359de932536f124
Robert Brown <brown.rjb75@gmail.com>
2022-09-28 00:03:45 -0600

    feat: school page and radio

---
+++ src/components/input/Radio.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Radio.scss";
+
+export interface RadioProps {
+    name: string,
+    label: string,
+    onChange?: (arg1: string, arg2: boolean) => void,
+    value: boolean,
+}
+
+const Radio: React.FC<RadioProps> = ({label, name, onChange, value}) => {
+    
+    const change = () => {
+        onChange && onChange(name, !value)
+    }
+
+    return (
+        <div className="radio-option" onClick={change}>
+            <div className="radio-option-container">
+                <input className={`radio-option-input${value ? ' selected' : ''}`} name={name} type={'radio'} value={name} />
+            </div>
+            <label htmlFor={name}>{label}</label>
+        </div>
+    )
+}
+
+export default Radio;
