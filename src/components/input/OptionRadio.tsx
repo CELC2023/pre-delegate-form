dd01b2db241775fa5e64417f5bbeb7df0e9c01e0
Robert Brown <brown.rjb75@gmail.com>
2022-09-28 23:09:20 -0600

    feat: option radio and degree info

---
+++ src/components/input/OptionRadio.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Radio from "./Radio";
+import "./OptionRadio.scss";
+import { Control, Controller } from "react-hook-form";
+
+export interface OptionRadioProps {
+  name: string;
+  label: string;
+  value?: string;
+  onChange?: (arg1: string) => void;
+  required?: boolean;
+  control?: Control<any>;
+  options: Array<any>;
+  horizontal?: boolean;
+}
+
+const OptionRadio: React.FC<OptionRadioProps> = ({
+  control,
+  label,
+  name,
+  onChange,
+  required = false,
+  value = [],
+  options,
+  horizontal = false,
+}) => {
+  const change = (radioName: string, value: boolean) => {
+    onChange && onChange(radioName)
+  };
+
+  return (
+    <>
+      {control ? (
+        <Controller
+          name={name}
+          control={control}
+          render={({ field: { name, onChange, value } }) => {
+            
+            const change = (radioName: string) => {
+                onChange(radioName)
+              };
+
+            return (
+              <div className="option-radio">
+                <p className="option-radio-label">
+                  {label}
+                  {required && "*"}
+                </p>
+                <div className={`option-radio-container${horizontal ? '' : ' vertical'}`}>
+                    {
+                        options.map((e, i) => <Radio name={e.name} label={e.label} value={value === e.name} onChange={change} key={i} />)
+                    } 
+                </div>
+              </div>
+            );
+          }}
+        />
+      ) : (
+        <div className="option-radio">
+          <p className="option-radio-label">
+            {label}
+            {required && "*"}
+          </p>
+          <div className={`option-radio-container${horizontal ? '' : ' vertical'}`}>
+            {
+                options.map((e, i) => <Radio name={e.name} label={e.label} value={value === e.name} onChange={change} key={i} />)
+            }
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default OptionRadio;
