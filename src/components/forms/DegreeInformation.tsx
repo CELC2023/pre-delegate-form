a91e4fd7b672ec2521e2f965f57f372ef453fc83
Robert Brown <brown.rjb75@gmail.com>
2022-09-28 23:09:20 -0600

    feat: option radio and degree info

--- src/components/forms/DegreeInformation.tsx
+++ src/components/forms/DegreeInformation.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import FormPageProps from "../../interfaces/FormPageProps";
 import Autocomplete from "../input/Autocomplete";
+import OptionRadio from "../input/OptionRadio";
 import TextInput from "../input/TextInput";
 import FormContent from "./FormContent";
 import FormNextButton from "./FormNextButton";
@@ -10,7 +11,7 @@ import FormPreviousButton from "./FormPreviousButton";
 
 const DegreeInformation: React.FC<FormPageProps> = ({onComplete, onBack}) => {
     const {t} = useTranslation();
-    const {control} = useForm();
+    const {control, watch} = useForm();
 
     const onNext = () => {
         onComplete && onComplete();
@@ -20,13 +21,34 @@ const DegreeInformation: React.FC<FormPageProps> = ({onComplete, onBack}) => {
         onBack && onBack();
     }
 
+    const degreeYears = [{
+        name: '1',
+        label: t('option-first-year')
+    }, {
+        name: '2',
+        label: t('option-second-year') 
+    }, {
+        name: '3',
+        label: t('option-third-year') 
+    }, {
+        name: '4',
+        label: t('option-fourth-year') 
+    }, {
+        name: '5',
+        label: t('option-fifth-year') 
+    }, {
+        name: '6',
+        label: t('option-sixth-year') 
+    }]
+
     return (
         <>
             <FormPreviousButton onClick={onPrevious} />
             <FormContent>
                 <h2>{t('text-school-info')}</h2>
-                <Autocomplete name="major" label={'field-major'} control={control} />
-                <TextInput name="minor" label={'field-minor'} />
+                <OptionRadio name="year" label={t('field-study-year')} options={degreeYears} control={control} value={watch('year')} />
+                <Autocomplete name="major" label={t('field-major')} control={control} />
+                <TextInput name="minor" label={t('field-minor')} />
             </FormContent>
             <FormNextButton onClick={onNext} />
         </>
