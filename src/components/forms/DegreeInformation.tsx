c5f794b30489ef0aa2949d7eabb463d773abea1f
Robert Brown <brown.rjb75@gmail.com>
2022-10-16 10:40:29 -0600

    feat: use slider for degree year

--- src/components/forms/DegreeInformation.tsx
+++ src/components/forms/DegreeInformation.tsx
@@ -6,7 +6,7 @@ import FormPageProps from "../../interfaces/FormPageProps";
 import { Major } from "../../models/major";
 import { selectDegreeMajor, selectDegreeMinor, selectDegreeYear, setDegreeInformation } from "../../redux/delegateReducer";
 import Autocomplete from "../input/Autocomplete";
-import OptionRadio from "../input/OptionRadio";
+import Slider from "../input/Slider";
 import TextInput from "../input/TextInput";
 import ProgressDots from "../ProgressDots";
 import FormContent from "./FormContent";
@@ -86,25 +86,7 @@ const DegreeInformation: React.FC<FormPageProps> = ({onComplete, onBack}) => {
         onBack && onBack();
     }
 
-    const degreeYears = [{
-        name: '1',
-        label: t('option-first-year')
-    }, {
-        name: '2',
-        label: t('option-second-year') 
-    }, {
-        name: '3',
-        label: t('option-third-year') 
-    }, {
-        name: '4',
-        label: t('option-fourth-year') 
-    }, {
-        name: '5',
-        label: t('option-fifth-year') 
-    }, {
-        name: '6',
-        label: t('option-sixth-year') 
-    }]
+    const marks = {1: "1", 2: "2", 3: "3", 4:"4", 5:"5", 6: "6+" }
 
     return (
         <>
@@ -112,9 +94,9 @@ const DegreeInformation: React.FC<FormPageProps> = ({onComplete, onBack}) => {
             <FormContent>
                 <ProgressDots steps={5} current={2} />
                 <h2>{t('text-school-info')}</h2>
-                <OptionRadio name="yearOfStudy" label={t('field-study-year')} options={degreeYears} control={control} value={watch('yearOfStudy')} />
                 <Autocomplete name="major" label={t('field-major')} control={control} options={majors} />
                 <TextInput name="minor" label={t('field-minor')} control={control} />
+                <Slider name="yearOfStudy" label={t('field-study-year')} control={control}  count={6} min={1} step={1} max={6} marks={marks} dots={true} />
             </FormContent>
             <FormNextButton onClick={onNext} />
         </>
--- src/components/input/Slider.tsx
+++ src/components/input/Slider.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import RCSlider, {SliderProps as RCSliderProps} from "rc-slider";
+import RCSlider, { SliderProps as RCSliderProps } from "rc-slider";
 import "./Slider.scss";
+import { Control, Controller } from "react-hook-form";
 
-const Slider: React.FC<RCSliderProps> = (props) => {
-    return (
-        <RCSlider className={`form-slider${props.className ? ' ' + props.className : ''}`} {...props} />
-    )
+interface SliderInputProps {
+  control?: Control<any>;
+  name: string;
+  label: string;
 }
 
-export default Slider
+export interface SliderProps extends RCSliderProps, SliderInputProps {}
+
+const Slider: React.FC<SliderProps> = (props) => {
+  return (
+    <>
+      <p>{props.label}</p>
+      {props?.control ? (
+        <Controller
+          name={props.name || ""}
+          control={props.control}
+          render={({ field: { onChange, value } }) => {
+            return (
+              <RCSlider
+                className={`form-slider${
+                  props.className ? " " + props.className : ""
+                }`}
+                onChange={onChange}
+                value={value}
+                {...props}
+              />
+            );
+          }}
+        />
+      ) : (
+        <RCSlider
+          className={`form-slider${
+            props.className ? " " + props.className : ""
+          }`}
+          {...props}
+        />
+      )}
+    </>
+  );
+};
+
+export default Slider;
