efd507259ba62b53418fc939b139f1bdae823779
Robert Brown <brown.rjb75@gmail.com>
2022-10-17 23:55:51 -0600

    feat: update file upload functionality

--- src/components/forms/Review.tsx
+++ src/components/forms/Review.tsx
@@ -1,36 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Trans, useTranslation } from "react-i18next";
 import FormPageProps from "../../interfaces/FormPageProps";
-import { selectDelegateData } from "../../redux/delegateReducer";
-import axiosInstance from "../../utils/axios";
-import { parseDelegateData } from "../../utils/datautils";
 import FormContent from "./FormContent";
-import FormNextButton from "./FormNextButton";
-import FormPreviousButton from "./FormPreviousButton";
 
-const Review: React.FC<FormPageProps> = ({onBack, onComplete}) => {
+const Review: React.FC<FormPageProps> = ({onBack}) => {
 
-    const formData = useSelector(selectDelegateData)
-    const navigate = useNavigate()
-    
-    const onNext = () => {
-        axiosInstance.post('/delegate/', parseDelegateData(formData))
-        .then(() => navigate("/delegate/complete", {replace: true}))
-        .catch((err) => console.error(err))
-    }
-
-    const onPrevious = () => {
-        onBack && onBack();
-    }
+    const {t} = useTranslation();
 
     return (
         <>
-            <FormPreviousButton onClick={onPrevious} />
             <FormContent>
-                <h2>Review</h2>
+                <h2>{t('text-form-complete')}</h2>
+                <p className="capital">{t('info-confirm-delegate')}</p>
+                <p><Trans i18nKey={'info-inquiries'}>FOR ANY INQUIRIES, PLEASE EMAIL <a href="mailto:chair@celc.cfes.ca">CHAIR@CELC.CFES.CA</a></Trans></p>
             </FormContent>
-            <FormNextButton onClick={onNext} />
         </>
     )
 }
